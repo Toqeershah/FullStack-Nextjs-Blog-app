719207798497d15e1268cd59e4d7c6ca7450d3ff
Toqeershah <toqeerabi85@gmail.com>
2024-09-16 12:12:39 +0500

    updated on sept 16 by me

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -2,11 +2,13 @@ import { blog_data } from '@/Assets/assets'
 import BlogItem from './BlogItem'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import { SignInButton, useUser } from '@clerk/nextjs';
 
 const BlogList = () => {
 
   const [menu, setMenu] = useState("All");
   const [blogs, setBlogs] = useState([]);
+  const { isSignedIn } = useUser(); // Hook to check if the user is logged in
 
   const fetchBlogs = async () => {
     const response = await axios.get('/api/blog');
@@ -20,12 +22,27 @@ const BlogList = () => {
 
   return (
     <div>
-      <div className='flex justify-center gap-6 my-10'>
+      {/* <div className='flex justify-center gap-6 my-10'>
         <button onClick={() => setMenu("All")} className={menu==="All" ? 'bg-black text-white py-1 px-4 rounded-sm': ""}>All</button>
         <button onClick={() => setMenu("Technology")} className={menu==="Technology" ? 'bg-black text-white py-1 px-4 rounded-sm': ""}>Technology</button>
         <button onClick={() => setMenu("Startup")} className={menu==="Startup" ? 'bg-black text-white py-1 px-4 rounded-sm': ""}>Startup</button>
         <button onClick={() => setMenu("Lifestyle")} className={menu==="Lifestyle" ? 'bg-black text-white py-1 px-4 rounded-sm': ""}>Lifestyle</button>
-      </div>
+      </div> */}
+      <div className='flex justify-center gap-6 my-10'>
+      {isSignedIn ? (
+        <>
+          <button onClick={() => setMenu("All")} className={menu === "All" ? 'bg-black text-white py-1 px-4 rounded-sm' : ""}>All</button>
+          <button onClick={() => setMenu("Technology")} className={menu === "Technology" ? 'bg-black text-white py-1 px-4 rounded-sm' : ""}>Technology</button>
+          <button onClick={() => setMenu("Startup")} className={menu === "Startup" ? 'bg-black text-white py-1 px-4 rounded-sm' : ""}>Startup</button>
+          <button onClick={() => setMenu("Lifestyle")} className={menu === "Lifestyle" ? 'bg-black text-white py-1 px-4 rounded-sm' : ""}>Lifestyle</button>
+        </>
+      ) : (
+        <></>
+        // <SignInButton mode="modal">
+        //   <button className='bg-black text-white py-1 px-4 rounded-sm'>Sign In</button>
+        // </SignInButton>
+      )}
+    </div>
       <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
         {blogs.filter((item) => menu==="All"?true:item.category===menu).map((item, index) => {
           return <BlogItem key={index} id={item._id} image={item.image} title={item.title} description={item.description} category={item.category} />
